1ed57ce14118bee4d3fff173f8a23d60572162f4
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-05-21 07:34:01 +0900

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
+
+import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+
+const config = defineConfig({
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    cloudflare({ viteEnvironment: { name: 'ssr' } }),
+    tailwindcss(),
+    tanstackStart(),
+    viteReact(),
+  ],
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      clientPort: 3000,
+    }
+  }
+})
+
+export default config
