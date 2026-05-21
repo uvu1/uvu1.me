1f1c59c06375d5bb98faf4c99c70a8bec52de737
uvu1 <53385458+uvu1@users.noreply.github.com>
2026-05-22 04:36:33 +0900

    cssをまとめた

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,6 +1,4 @@
 import { HeadContent, Scripts, createRootRoute } from '@tanstack/react-router'
-import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
 import { siteConfig }from '../config/site'
 import { NotFoundPage }from "../components/error/NotFoundPage"
 import { ThemeScript } from '../components/theme/ThemeScript'
@@ -103,17 +101,6 @@ function RootDocument({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        <TanStackDevtools
-          config={{
-            position: 'bottom-right',
-          }}
-          plugins={[
-            {
-              name: 'Tanstack Router',
-              render: <TanStackRouterDevtoolsPanel />,
-            },
-          ]}
-        />
         <Scripts />
       </body>
     </html>
